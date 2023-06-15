1190d493472f9fc35e1d53de2a9545faecf6cbb4
Donshaker <oskob20@gmail.com>
2023-06-15 16:10:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A c program that print a line to the standard error
  * Return 1 (Success)
  */
-intmain(void)
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", stdout);
 	return (1);
