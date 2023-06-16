fa7cba2de23dd9c906c7fec6ce6be414543b0366
Donshaker <oskob20@gmail.com>
2023-06-16 13:57:52 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,13 @@
  *
  * Return: 0 on success
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n = rand() RAND_MAX / 2;
+	n = rand() -  RAND_MAX / 2;
 	/* your code goes there */
 	if (n < 0)
 	{
