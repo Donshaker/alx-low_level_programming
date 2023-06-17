6a073c72b7422a29c3750bcffc2b2d7a17a190cc
Donshaker <oskob20@gmail.com>
2023-06-16 22:50:09 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 9; i++)
+    {
+        putchar(i + '0'); /* Print the current number */
+
+        if (i != 8) /* Check if it's not the last number */
+        {
+		putchar(','); /* Print the comma */
+            putchar(' '); /* Print the space */
+        }
+    }
+
+    putchar('\n'); /* Print a newline character */
+
+    return 0;
+}
