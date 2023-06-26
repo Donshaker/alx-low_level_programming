28934e31384387fa3b042be4441e6abe6063858a
Donshaker <oskob20@gmail.com>
2023-06-26 16:06:02 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_string - Prints a string followed by a new line
+ * @str: The string to be printed
+ */
+void print_string(const char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
+
