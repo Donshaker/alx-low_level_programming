617453f6c2cbd20e75fdf603f99f7fa22aee7922
Donshaker <oskob20@gmail.com>
2023-07-03 21:19:52 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * _strpbrk - Locates the first occurrence of any byte from accept in s
- * @s: The string to be searched
- * @accept: The string containing bytes to be matched
- *
- * Return: A pointer to the matching byte in s, or NULL if no match is found
- */
-char *_strpbrk(char *s, char *accept)
+int main(void)
 {
-    while (*s != '\0')
-    {
-        char *current_accept = accept;
-        while (*current_accept != '\0')
-        {
-            if (*s == *current_accept)
-                return s;
-            current_accept++;
-        }
-        s++;
-    }
-    return (NULL);
+    char str[] = "Hello, World!";
+    char accept[] = "aeiou";
+    char *result = _strpbrk(str, accept);
+
+    if (result)
+        printf("First vowel found in the string: %c\n", *result);
+    else
+        printf("No vowel found in the string.\n");
+
+    return (0);
 }
+
 
