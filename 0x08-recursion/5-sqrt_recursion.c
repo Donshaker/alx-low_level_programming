167c11a0b027448419bf811c5408c5330d43815a
Donshaker <oskob20@gmail.com>
2023-07-05 15:43:26 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,34 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
+ * sqrt_helper - Recursive helper function to calculate square root
  * @n: The number to calculate the square root of
+ * @guess: The current guess for the square root
  *
- * Return: The natural square root of the number, or -1 if it does not have a natural square root
+ * Return: The natural square root of n, or -1 if it doesn't exist
  */
-int _sqrt_recursion(int n)
+int sqrt_helper(int n, int guess)
 {
-if (n < 0)  /* Error case: n is negative */
-return -1;
+if (guess * guess == n)
+return (guess); /* Found the exact square root */
 
-return sqrt_helper(0);
+if (guess * guess > n)
+return (-1); /* Square root doesn't exist */
+
+return (sqrt_helper(n, guess + 1)); /* Increment the guess and continue */
 }
 
 /**
- * sqrt_helper - Recursive helper function to calculate the square root
+ * _sqrt_recursion - Calculates the natural square root of a number
  * @n: The number to calculate the square root of
- * @low: The lower bound for searching
- * @high: The upper bound for searching
  *
- * Return: The square root of the number, or -1 if it does not have a natural square root
+ * Return: The natural square root of n, or -1 if it doesn't exist
  */
-int sqrt_helper(int n, int low, int high)
+int _sqrt_recursion(int n)
 {
-int mid = (low + high) / 2;
+if (n < 0)
+return (-1); /* Square root doesn't exist for negative numbers */
 
-if (low > high)  /* Base case: no natural square root found */
-return -1;
-
-if (mid * mid == n)  /* Base case: natural square root found */
-return mid;
-
-if (mid * mid > n)  /* Search in the lower half */
-return sqrt_helper(n, low, mid - 1);
-
-/* Search in the upper half */
-return sqrt_helper(n, mid + 1, high);
+return (sqrt_helper(n, 0));
 }
 
