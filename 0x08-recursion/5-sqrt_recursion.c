bb040ceb962e4ed6f297d708d72840105f3c8e30
Donshaker <oskob20@gmail.com>
2023-07-05 13:15:46 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,24 +11,31 @@ int _sqrt_recursion(int n)
 if (n < 0)  /* Error case: n is negative */
 return -1;
 
-return sqrt_helper(n, 0);
+return sqrt_helper(n, 0, n);
 }
 
 /**
  * sqrt_helper - Recursive helper function to calculate the square root
  * @n: The number to calculate the square root of
- * @i: The current value to check
+ * @low: The lower bound for searching
+ * @high: The upper bound for searching
  *
  * Return: The square root of the number, or -1 if it does not have a natural square root
  */
-int sqrt_helper(int n, int i)
+int sqrt_helper(int n, int low, int high)
 {
-if (i * i > n)  /* Base case: no natural square root found */
+int mid = (low + high) / 2;
+
+if (low > high)  /* Base case: no natural square root found */
 return -1;
 
-if (i * i == n)  /* Base case: natural square root found */
-return i;
+if (mid * mid == n)  /* Base case: natural square root found */
+return mid;
 
-return sqrt_helper(n, i + 1);  /* Recursively check the next value */
+if (mid * mid > n)  /* Search in the lower half */
+return sqrt_helper(n, low, mid - 1);
+
+/* Search in the upper half */
+return sqrt_helper(n, mid + 1, high);
 }
 
