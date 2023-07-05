ea4c35189d9b9e12b0f87d4a3055422bacb94042
Donshaker <oskob20@gmail.com>
2023-07-05 15:33:37 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * compare_chars - Compares characters recursively from both ends of the string
+ * @s: The string to check
+ * @start: The starting index of the substring to compare
+ * @end: The ending index of the substring to compare
+ *
+ * Return: 1 if the substring is a palindrome, 0 otherwise
+ */
+int compare_chars(char *s, int start, int end)
+{
+if (start >= end)
+return (1); /* Substring is a palindrome */
+
+if (s[start] != s[end])
+return (0); /* Substring is not a palindrome */
+
+return (compare_chars(s, start + 1, end - 1)); /* Recursive call */
+}
+
+/**
  * is_palindrome - Checks if a string is a palindrome
  * @s: The string to check
  *
@@ -9,7 +28,6 @@
 int is_palindrome(char *s)
 {
 int len = 0;
-int i;
 
 /* Find the length of the string */
 while (s[len] != '\0')
@@ -19,14 +37,7 @@ len++;
 if (len == 0)
 return (1);
 
-/* Compare characters from both ends */
-for (i = 0; i < len / 2; i++)
-{
-if (s[i] != s[len - 1 - i])
-return (0);
-}
-
-return (1);
+return (compare_chars(s, 0, len - 1));
 }
 
 
