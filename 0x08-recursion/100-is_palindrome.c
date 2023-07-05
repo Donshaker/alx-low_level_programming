46edddcdf4817b766598a2604db5d3db6a75cd2e
Donshaker <oskob20@gmail.com>
2023-07-05 13:40:58 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,25 +13,24 @@ int is_palindrome(char *s)
     if (len <= 1)  /* Base case: empty string or single character is a palindrome */
         return 1;
 
-    return is_palindrome_helper(s, 0, len - 1);
+    return is_palindrome_helper(s, s + len - 1);
 }
 
 /**
  * is_palindrome_helper - Recursive helper function to check if a string is a palindrome
- * @s: The string to check
- * @start: The starting index of the substring to check
- * @end: The ending index of the substring to check
+ * @start: The starting character of the substring to check
+ * @end: The ending character of the substring to check
  *
  * Return: 1 if the string is a palindrome, 0 otherwise
  */
-int is_palindrome_helper(char *s, int start, int end)
+int is_palindrome_helper(char *start, char *end)
 {
     if (start >= end)  /* Base case: substring is a palindrome */
         return 1;
 
-    if (s[start] != s[end])  /* Base case: substring is not a palindrome */
+    if (*start != *end)  /* Base case: substring is not a palindrome */
         return 0;
 
-    return is_palindrome_helper(s, start + 1, end - 1);
+    return is_palindrome_helper(start + 1, end - 1);
 }
 
