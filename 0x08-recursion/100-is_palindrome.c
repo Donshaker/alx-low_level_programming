3c0b3d559b5168d353e9fd3ab6fa0b4ceda7b032
Donshaker <oskob20@gmail.com>
2023-07-05 15:53:27 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _strlen - Calculates the length of a string recursively
+ * @s: The string to calculate the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+
+return (1 + _strlen(s + 1));
+}
+
+/**
  * compare_chars - Compares characters recursively from both ends of the string
  * @s: The string to check
  * @start: The starting index of the substring to compare
@@ -27,11 +41,7 @@ return (compare_chars(s, start + 1, end - 1)); /* Recursive call */
  */
 int is_palindrome(char *s)
 {
-int len = 0;
-
-/* Find the length of the string */
-while (s[len] != '\0')
-len++;
+int len = _strlen(s);
 
 /* Check if the string is empty */
 if (len == 0)
@@ -39,5 +49,4 @@ return (1);
 
 return (compare_chars(s, 0, len - 1));
 }
-
 
