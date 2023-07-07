96f9418c27e728627275f93b6766ab5f0bb7b0d5
Donshaker <oskob20@gmail.com>
2023-07-07 13:20:02 -0700

    0-whatsmyname.c

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,11 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(int argc, char *argv[])
 {
-(void)argc; /* Unused parameter */
-    
-printf("%s\n", argv[0]);
+    (void)argc; /* Unused parameter */
 
-return (0);
+    print_program_name(argv);
+
+    return 0;
+}
+
+void print_program_name(char *argv[])
+{
+    int i = 0;
+
+    while (argv[0][i] != '\0')
+    {
+        _putchar(argv[0][i]);
+        i++;
+    }
+    _putchar('\n');
+}
+
+int _putchar(char c)
+{
+    return putchar(c);
 }
 
