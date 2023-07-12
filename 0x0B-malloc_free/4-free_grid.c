634936691b18d48f134020c5bc3134018ed6cf68
Donshaker <oskob20@gmail.com>
2023-07-11 22:26:44 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+void free_grid(int **grid, int height)
+{
+    if (grid == NULL)
+        return;
+
+    for (int i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+
+    free(grid);
+}
+
