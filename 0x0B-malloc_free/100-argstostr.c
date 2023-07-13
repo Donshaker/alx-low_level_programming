f3f18f2fdfab512f3f66967d6f42b143f950a441
Donshaker <oskob20@gmail.com>
2023-07-12 21:50:05 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,49 +1,80 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 
 /**
- * argstostr - Concatenates all the arguments of a program.
- * @ac: The number of arguments.
- * @av: An array of strings containing the arguments.
- *
- * Return: A pointer to the new string, or NULL if it fails.
- *         Each argument is followed by a newline character '\n'.
+ * free_words - Frees the memory allocated for an array of strings.
+ * @words: The array of strings to be freed.
+ * @len: The length of the array.
  */
-char *argstostr(int ac, char **av)
+void free_words(char **words, int len)
 {
-	int i, j, k = 0, length = 0;
-	char *str;
+	int i;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+	for (i = 0; i < len; i++)
+		free(words[i]);
 
-	/* Calculate the length of the new string */
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			length++;
-		length++; /* Account for the newline character */
-	}
-
-	str = malloc((length + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-
-	/* Copy the arguments into the new string */
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n'; /* Append the newline character */
-		k++;
-	}
-
-	str[k] = '\0'; /* Add the null terminator at the end */
-	return (str);
+	free(words);
 }
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The input string.
+ *
+ * Return: A pointer to an array of strings (words) or NULL if it fails.
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int i, j, k, len, count = 0, word_len = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	len = strlen(str);
+
+	/* Count the number of words in the string */
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] != ' ')
+		{
+			word_len++;
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+				count++;
+		}
+	}
+
+	if (count == 0)
+		return (NULL);
+
+	words = malloc((count + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+
+	/* Allocate memory for each word and copy the characters */
+	j = 0;
+	for (i = 0; i < len && j < count; i++)
+	{
+		if (str[i] != ' ')
+		{
+			word_len++;
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+			{
+				words[j] = malloc((word_len + 1) * sizeof(char));
+				if (words[j] == NULL)
+				{
+					free_words(words, j);
+					return (NULL);
+				}
+				strncpy(words[j], str + i - word_len + 1, word_len);
+				words[j][word_len] = '\0';
+				j++;
+				word_len = 0;
+			}
+		}
+	}
+
+	words[count] = NULL;
+	return (words);
+}
+
 
