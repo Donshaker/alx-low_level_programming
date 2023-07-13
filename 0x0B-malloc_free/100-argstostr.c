3df630f7f75875e6e7339f2079105b32b351cf6e
Donshaker <oskob20@gmail.com>
2023-07-12 22:41:08 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,80 +1,45 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
-/**
- * free_words - Frees the memory allocated for an array of strings.
- * @words: The array of strings to be freed.
- * @len: The length of the array.
- */
-void free_words(char **words, int len)
+char *argstostr(int ac, char **av);
+
+char *argstostr(int ac, char **av)
 {
-	int i;
+    if (ac == 0 || av == NULL)
+        return NULL;
 
-	for (i = 0; i < len; i++)
-		free(words[i]);
+    int total_length = 0;
+    int i, j;
+    for (i = 0; i < ac; i++)
+    {
+        j = 0;
+        while (av[i][j] != '\0')
+        {
+            total_length++;
+            j++;
+        }
+        total_length++; // Account for the newline character
+    }
 
-	free(words);
+    char *result = malloc((total_length + 1) * sizeof(char));
+    if (result == NULL)
+        return NULL;
+
+    int index = 0;
+    for (i = 0; i < ac; i++)
+    {
+        j = 0;
+        while (av[i][j] != '\0')
+        {
+            result[index] = av[i][j];
+            index++;
+            j++;
+        }
+        result[index] = '\n';
+        index++;
+    }
+    result[index] = '\0';
+
+    return result;
 }
-
-/**
- * strtow - Splits a string into words.
- * @str: The input string.
- *
- * Return: A pointer to an array of strings (words) or NULL if it fails.
- */
-char **strtow(char *str)
-{
-	char **words;
-	int i, j, k, len, count = 0, word_len = 0;
-
-	if (str == NULL || *str == '\0')
-		return (NULL);
-
-	len = strlen(str);
-
-	/* Count the number of words in the string */
-	for (i = 0; i < len; i++)
-	{
-		if (str[i] != ' ')
-		{
-			word_len++;
-			if (str[i + 1] == ' ' || str[i + 1] == '\0')
-				count++;
-		}
-	}
-
-	if (count == 0)
-		return (NULL);
-
-	words = malloc((count + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
-
-	/* Allocate memory for each word and copy the characters */
-	j = 0;
-	for (i = 0; i < len && j < count; i++)
-	{
-		if (str[i] != ' ')
-		{
-			word_len++;
-			if (str[i + 1] == ' ' || str[i + 1] == '\0')
-			{
-				words[j] = malloc((word_len + 1) * sizeof(char));
-				if (words[j] == NULL)
-				{
-					free_words(words, j);
-					return (NULL);
-				}
-				strncpy(words[j], str + i - word_len + 1, word_len);
-				words[j][word_len] = '\0';
-				j++;
-				word_len = 0;
-			}
-		}
-	}
-
-	words[count] = NULL;
-	return (words);
-}
-
 
