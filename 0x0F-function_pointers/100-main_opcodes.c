9b3dd72c3e46d8f9a354e936f395f826f8269b27
Donshaker <oskob20@gmail.com>
2023-07-19 15:26:52 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	/* Print the opcodes */
+	for (i = 0; i < bytes; i++)
+		printf("%02x ", ((unsigned char *)main)[i]);
+
+	printf("\n");
+	return (0);
+}
+
