3bb1e737253cc451a8b4ce9932619eb6b60ccbd0
Donshaker <oskob20@gmail.com>
2023-07-19 15:40:15 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/* Function to perform addition */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/* Function to perform subtraction */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/* Function to perform multiplication */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/* Function to perform division */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/* Function to perform modulo */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
