e64d67c7307cb5dec3f9c4d96ffec1541e33d335
Donshaker <oskob20@gmail.com>
2023-07-19 15:38:56 -0700

    3-calc.h

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,19 @@
+#ifndef CALC_H
+#define CALC_H
+
+/* Structure for operation and function mapping */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+/* Function prototypes */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif /* CALC_H */
+
