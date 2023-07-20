eadfad8e1de7ec56603ba20242e9dda2acb79c89
Donshaker <oskob20@gmail.com>
2023-07-19 22:52:31 -0700

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,34 +2,75 @@
 #include <stdlib.h>
 #include "calc.h"
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success, 98 if number of arguments is wrong,
+ *         99 if the operator is invalid, 100 if division by 0 occurs.
+ */
 int main(int argc, char *argv[])
 {
+    int num1, num2;
+    int result;
+    int (*func_ptr)(int, int);
+
     if (argc != 4)
     {
-        printf("Error\n");
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
         return 98;
     }
 
-    int num1 = atoi(argv[1]);
-    char operator = argv[2][0];
-    int num2 = atoi(argv[3]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-    int result;
-    if (!validate_operator(operator))
+    if (argv[2][1] != '\0' || (argv[2][0] != '+' && argv[2][0] != '-' &&
+                               argv[2][0] != '*' && argv[2][0] != '/' &&
+                               argv[2][0] != '%'))
     {
-        printf("Error\n");
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
         return 99;
     }
 
-    if ((operator == '/' || operator == '%') && num2 == 0)
+    func_ptr = get_op_func(argv[2]);
+
+    if (func_ptr == NULL)
     {
-        printf("Error\n");
-        return (100);
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
+        return 99;
     }
 
-    result = perform_operation(num1, operator, num2);
-    printf("%d\n", result);
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+    {
+        _putchar('E');
+        _putchar('r');
+        _putchar('r');
+        _putchar('o');
+        _putchar('r');
+        _putchar('\n');
+        return 100;
+    }
 
-    return (0);
+    result = func_ptr(num1, num2);
+    _putchar(result + '0');
+    _putchar('\n');
+
+    return 0;
 }
 
