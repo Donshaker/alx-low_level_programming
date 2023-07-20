547bc0dda01cbe899c7627653b3b5b0f0bbfbd16
Donshaker <oskob20@gmail.com>
2023-07-19 22:26:31 -0700

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
+#include "calc.h"
 
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: Always 0 (Success)
- */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*operation)(int, int);
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return 98;
+    }
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    int num1 = atoi(argv[1]);
+    char operator = argv[2][0];
+    int num2 = atoi(argv[3]);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	operation = get_op_func(argv[2]);
+    int result;
+    if (!validate_operator(operator))
+    {
+        printf("Error\n");
+        return 99;
+    }
 
-	if (operation == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    if ((operator == '/' || operator == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+    result = perform_operation(num1, operator, num2);
+    printf("%d\n", result);
 
-	result = operation(num1, num2);
-	printf("%d\n", result);
-
-	return (0);
+    return (0);
 }
 
