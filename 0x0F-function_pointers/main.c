fc735fa5a78978223700e771afb87f9059e56a77
Donshaker <oskob20@gmail.com>
2023-07-19 22:06:36 -0700

    main

---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return 98;
+    }
+
+    int num1 = atoi(argv[1]);
+    char operator = argv[2][0];
+    int num2 = atoi(argv[3]);
+
+    int result;
+    if (!validate_operator(operator))
+    {
+        printf("Error\n");
+        return 99;
+    }
+
+    if ((operator == '/' || operator == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return 100;
+    }
+
+    result = perform_operation(num1, operator, num2);
+    printf("%d\n", result);
+
+    return 0;
+}
+
