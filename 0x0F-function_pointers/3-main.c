303ca5330750c070596a5ee700514cb1de6484cf
Donshaker <oskob20@gmail.com>
2023-07-20 19:18:40 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,76 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: Array of command-line argument strings
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  *
- * Return: 0 on success, 98 if number of arguments is wrong,
- *         99 if the operator is invalid, 100 if division by 0 occurs.
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int result;
-    int (*func_ptr)(int, int);
+    int num1, num2, result;
+    int (*op_func)(int, int);
 
     if (argc != 4)
     {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return 98;
+        printf("Error\n");
+        return (98);
     }
 
     num1 = atoi(argv[1]);
     num2 = atoi(argv[3]);
 
-    if (argv[2][1] != '\0' || (argv[2][0] != '+' && argv[2][0] != '-' &&
-                               argv[2][0] != '*' && argv[2][0] != '/' &&
-                               argv[2][0] != '%'))
-    {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return 99;
-    }
+    op_func = get_op_func(argv[2]);
 
-    func_ptr = get_op_func(argv[2]);
-
-    if (func_ptr == NULL)
+    if (op_func == NULL)
     {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return 99;
+        printf("Error\n");
+        return (99);
     }
 
     if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
     {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return 100;
+        printf("Error\n");
+        return (100);
     }
 
-    result = func_ptr(num1, num2);
-    _putchar(result + '0');
-    _putchar('\n');
+    result = op_func(num1, num2);
+    printf("%d\n", result);
 
-    return 0;
+    return (0);
 }
 
