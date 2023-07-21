db06aa49f31a11bf16869dcbd1d7d9aa83f756e5
Donshaker <oskob20@gmail.com>
2023-07-20 19:35:51 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,61 @@
 #include "3-calc.h"
 
 /**
- * add - Addition operation
+ * op_add - Addition operation
  * @a: First operand
  * @b: Second operand
  *
  * Return: Result of the addition
  */
-int add(int a, int b)
+int op_add(int a, int b)
 {
     return (a + b);
 }
 
 /**
- * sub - Subtraction operation
+ * op_sub - Subtraction operation
  * @a: First operand
  * @b: Second operand
  *
  * Return: Result of the subtraction
  */
-int sub(int a, int b)
+int op_sub(int a, int b)
 {
     return (a - b);
 }
 
 /**
- * mul - Multiplication operation
+ * op_mul - Multiplication operation
  * @a: First operand
  * @b: Second operand
  *
  * Return: Result of the multiplication
  */
-int mul(int a, int b)
+int op_mul(int a, int b)
 {
     return (a * b);
 }
 
 /**
- * div_op - Division operation
+ * op_div - Division operation
  * @a: First operand
  * @b: Second operand
  *
  * Return: Result of the division
  */
-int div_op(int a, int b)
+int op_div(int a, int b)
 {
     return (a / b);
 }
 
 /**
- * mod - Modulo operation
+ * op_mod - Modulo operation
  * @a: First operand
  * @b: Second operand
  *
  * Return: Result of the modulo operation
  */
-int mod(int a, int b)
+int op_mod(int a, int b)
 {
     return (a % b);
 }
