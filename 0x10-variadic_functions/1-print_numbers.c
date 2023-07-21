17b0860c7f508381bd1c9f64d1b1f0a73a6502a4
Donshaker <oskob20@gmail.com>
2023-07-20 19:00:53 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,38 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Prints numbers, followed by a new line.
+ * print_numbers - Prints numbers followed by a new line.
  * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
- *
- * Return: void
+ * @...: Variable number of integers to be printed.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i;
-	int num;
+    va_list args;
+    unsigned int i;
+    int current_num;
 
-	/* Initialize va_list */
-	va_start(args, n);
+    va_start(args, n);
 
-	for (i = 0; i < n; i++)
-	{
-		/* Get the next argument */
-		num = va_arg(args, int);
+    for (i = 0; i < n; i++)
+    {
+        current_num = va_arg(args, int);
 
-		/* Print the number */
-		printf("%d", num);
+        printf("%d", current_num);
 
-		/* Print the separator if it's not NULL and not the last number */
-		if (separator != NULL && i < n - 1)
-			printf("%s", separator);
-	}
+        if (separator != NULL && i < n - 1)
+            printf("%s", separator);
+    }
 
-	/* Clean up the va_list */
-	va_end(args);
-
-	/* Print a new line at the end */
-	printf("\n");
+    va_end(args);
+    printf("\n");
 }
 
