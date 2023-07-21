5e962d42ce327e0843723fae216352fb9abbfcc0
Donshaker <oskob20@gmail.com>
2023-07-20 19:34:09 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,7 +1,7 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _3_CALC_H_
+#define _3_CALC_H_
 
-/* Structure to hold operator and function mapping */
+/* Structure to hold operator and function mappings */
 typedef struct op
 {
     char *op;
@@ -14,7 +14,8 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
-#endif /* CALC_H */
+#endif /* _3_CALC_H_ */
+
 
