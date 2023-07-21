0dbac75213e8bd4b09661e5adbce5931ed6f115e
Donshaker <oskob20@gmail.com>
2023-07-20 18:53:48 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,47 +3,48 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - Prints anything based on the format provided.
- * @format: A list of types of arguments passed to the function.
- *          c: char
- *          i: integer
- *          f: float
- *          s: char* (if the string is NULL, print (nil) instead)
- *          Any other char should be ignored.
+ * print_all - Prints anything based on the format.
+ * @format: The list of types of arguments passed to the function.
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead)
  */
 void print_all(const char * const format, ...)
 {
     va_list args;
     unsigned int i = 0;
+    char current_char;
     char *separator = "";
+    char *str;
 
     va_start(args, format);
 
     while (format && format[i])
     {
-        if (i > 0)
-            separator = ", ";
+        current_char = format[i];
 
-        switch (format[i])
+        if (current_char == 'c')
         {
-            case 'c':
-                printf("%s%c", separator, va_arg(args, int));
-                break;
-            case 'i':
-                printf("%s%d", separator, va_arg(args, int));
-                break;
-            case 'f':
-                printf("%s%f", separator, va_arg(args, double));
-                break;
-            case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(nil)";
-                    printf("%s%s", separator, str);
-                }
-                break;
+            printf("%s%c", separator, va_arg(args, int));
         }
+        else if (current_char == 'i')
+        {
+            printf("%s%d", separator, va_arg(args, int));
+        }
+        else if (current_char == 'f')
+        {
+            printf("%s%f", separator, va_arg(args, double));
+        }
+        else if (current_char == 's')
+        {
+            str = va_arg(args, char *);
+            if (str == NULL)
+                str = "(nil)";
+            printf("%s%s", separator, str);
+        }
+
+        separator = ", ";
         i++;
     }
 
