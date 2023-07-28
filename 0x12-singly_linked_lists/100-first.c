56e77e43f7ed6f12d9f8f94bc60d9cf63dd6247e
Donshaker <oskob20@gmail.com>
2023-07-28 01:04:06 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include "lists.h"
 
+/* Function prototype for the constructor */
+void print_message(void) __attribute__((constructor));
+
 /**
- * before_main - Function to be executed before main function.
+ * print_message - Function to be executed before main function.
  *
  * Description: This function prints the specified message before
  * the main function is executed.
  */
-void before_main(void)
+void print_message(void)
 {
     printf("You're beat! and yet, you must allow,\n"
            "I bore my house upon my back!\n");
 }
 
+int main(void)
+{
+    /* Add test code or the rest of your program here */
 
-
+    return 0;
+}
 
