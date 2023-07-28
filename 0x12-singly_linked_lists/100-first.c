d1bb63ca6a20a5c282a7fdc3820199ebc0c98927
Donshaker <oskob20@gmail.com>
2023-07-28 01:52:53 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,7 +12,7 @@ void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n"
-	       "(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	       "(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n\n");
 }
 
 
