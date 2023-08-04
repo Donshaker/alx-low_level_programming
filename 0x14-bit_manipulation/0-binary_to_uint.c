1d63cc998debcb6b6c3ff7151a99522e31931a64
Donshaker <oskob20@gmail.com>
2023-08-03 20:16:17 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,24 +10,21 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i])
+	while (*b)
 	{
-		if (b[i] == '0' || b[i] == '1')
+		if (*b == '0' || *b == '1')
 		{
-			result <<= 1;
-			if (b[i] == '1')
-				result |= 1;
+			result = (result << 1) + (*b - '0');
 		}
 		else
 		{
 			return (0);
 		}
-		i++;
+		b++;
 	}
 
 	return (result);
