eae4fa21b32f7aab748733fc5f156f35b071c760
Donshaker <oskob20@gmail.com>
2023-08-08 22:05:20 -0700

    elf header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,53 +4,52 @@
 #include <unistd.h>
 #include <elf.h>
 
-#define BUF_SIZE 64
-
-void print_error(const char *msg) {
-    fprintf(stderr, "Error: %s\n", msg);
-    exit(98);
-}
-
-void print_elf_header_info(int fd) {
-    Elf64_Ehdr elf_header;
-
-    if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
-        print_error("Failed to read ELF header");
-
-    printf("Magic: %02x %02x %02x %02x\n", elf_header.e_ident[EI_MAG0], elf_header.e_ident[EI_MAG1], elf_header.e_ident[EI_MAG2], elf_header.e_ident[EI_MAG3]);
-    printf("Class: %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-    printf("Data: %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little-endian" : "2's complement, big-endian");
-    printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
-    printf("OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
-    printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
-    printf("Type: %d\n", elf_header.e_type);
-    printf("Entry point address: 0x%lx\n", (unsigned long) elf_header.e_entry);
+void print_elf_header_info(Elf64_Ehdr *elf_header) {
+    printf("Magic: ");
+    for (int i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", elf_header->e_ident[i]);
+    }
+    printf("\nClass: %d\n", elf_header->e_ident[EI_CLASS]);
+    printf("Data: %d\n", elf_header->e_ident[EI_DATA]);
+    printf("Version: %d\n", elf_header->e_ident[EI_VERSION]);
+    printf("OS/ABI: %d\n", elf_header->e_ident[EI_OSABI]);
+    printf("ABI Version: %d\n", elf_header->e_ident[EI_ABIVERSION]);
+    printf("Type: %d\n", elf_header->e_type);
+    printf("Entry point address: 0x%lx\n", (unsigned long)elf_header->e_entry);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        return 1;
+        exit(98);
     }
 
     int fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        print_error("Failed to open ELF file");
-
-    Elf64_Ehdr elf_header;
-    if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
-        print_error("Failed to read ELF header");
-
-    if (elf_header.e_ident[EI_MAG0] != ELFMAG0 || elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-        elf_header.e_ident[EI_MAG2] != ELFMAG2 || elf_header.e_ident[EI_MAG3] != ELFMAG3)
-    {
-        print_error("Not an ELF file");
+    if (fd == -1) {
+        fprintf(stderr, "Error: Unable to open file %s\n", argv[1]);
+        exit(98);
     }
 
-    print_elf_header_info(fd);
+    Elf64_Ehdr elf_header;
+    ssize_t bytes_read = read(fd, &elf_header, sizeof(Elf64_Ehdr));
+    if (bytes_read != sizeof(Elf64_Ehdr)) {
+        fprintf(stderr, "Error: Unable to read ELF header\n");
+        close(fd);
+        exit(98);
+    }
+
+    if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
+        elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+        elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
+        elf_header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Error: Not an ELF file\n");
+        close(fd);
+        exit(98);
+    }
+
+    print_elf_header_info(&elf_header);
 
     close(fd);
-    return 0;
-}
+    return (0);
 }
 
